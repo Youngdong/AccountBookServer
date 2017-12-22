e7eff9753a296162af887032ae99d8d244b56038
Youngdong An <youngdongan@gmail.com>
2017-12-22 20:03:19 +0900

    Fix hazelcast setting

--- src/main/java/com/flycamel/accountbookserver/AccountBookServerApplication.java
+++ src/main/java/com/flycamel/accountbookserver/AccountBookServerApplication.java
@@ -1,17 +1,11 @@
 package com.flycamel.accountbookserver;
 
 import com.flycamel.accountbookserver.api.vertx.UserApiVerticle;
-import com.hazelcast.client.HazelcastClient;
-import com.hazelcast.client.config.ClientConfig;
-import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.spring.cache.HazelcastCacheManager;
 import io.vertx.core.Vertx;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
@@ -23,16 +17,9 @@ public class AccountBookServerApplication {
 
 	private UserApiVerticle userApiVerticle;
 
-	private CacheManager cacheManager;
-
 	@Resource
 	public void setUserApiVerticle(UserApiVerticle userApiVerticle) {
 		this.userApiVerticle = userApiVerticle;
-	}
-
-	@Resource
-	public void setCacheManager(CacheManager cacheManager) {
-		this.cacheManager = cacheManager;
 	}
 
 	public static void main(String[] args) {
@@ -41,26 +28,8 @@ public class AccountBookServerApplication {
 
 	@PostConstruct
 	public void deployVerticle() {
-		log.info("Cache manager: " + cacheManager);
-		log.info("Cache manager names: " + cacheManager.getCacheNames());
-
 		Vertx vertx = Vertx.vertx();
 
 		vertx.deployVerticle(userApiVerticle);
-	}
-
-	@Bean
-	HazelcastInstance hazelcastCacheInstance() {
-		ClientConfig config = new ClientConfig();
-		config.getGroupConfig().setName("dev").setPassword("dev-pass");
-		config.getNetworkConfig().addAddress("localhost");
-		config.setInstanceName("hazelcastQueryCache");
-		HazelcastInstance instance = HazelcastClient.newHazelcastClient(config);
-		return instance;
-	}
-
-	@Bean
-	CacheManager cacheManager() {
-		return new HazelcastCacheManager(hazelcastCacheInstance());
 	}
 }
